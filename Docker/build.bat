25c6e492cf12d02eaf52593d6817190704ae4749
adejokeajibade <adejokeajibade01@gmail.com>
2023-03-10 00:05:52 +0100

    Delete Docker directory

--- Docker/build.bat
+++
@@ -1,2 +0,0 @@
-docker-compose up -d --build
-docker-compose exec -w /var/www redprint_app bash /var/www/Docker/redprint.sh
--- Docker/redprint.sh
+++
@@ -1,6 +0,0 @@
-cp .env.example.docker .env && \
-	composer install && \
-    php artisan key:generate && \
-    php artisan migrate && \
-    php artisan db:seed && \
-    php artisan permissible:setup
