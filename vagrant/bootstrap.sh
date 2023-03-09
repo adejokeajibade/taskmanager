3269d777cc01a0ca008b4909f39f52878f8b3aac
adejokeajibade <adejokeajibade01@gmail.com>
2023-03-10 00:07:45 +0100

    Delete vagrant directory

--- vagrant/bootstrap.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-sudo service nginx restart
-sudo service php7.3-fpm restart
-
-# supervisor
-sudo supervisorctl reread
-sudo supervisorctl update
-sudo supervisorctl start laravel-worker:*
